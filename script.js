eede28a9801dc359d3deb5ca9b591914d698aba3
wesley belarmino <wesleybelarmino23@gmail.com>
2025-06-30 15:30:38 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+// Seleciona o formulário e a mensagem de sucesso
 
   const form = document.getElementById("form-contato");
   const mensagem = document.getElementById("mensagem-sucesso");
 
+  // Escuta o evento de envio do formulário
+
   form.addEventListener("submit", function(e){
     e.preventDefault();
-    mensagem.style.display = "block";
-    form.reset(); 
+  // Pega os valores dos campos
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let mensagem = document.getElementById("mensagem").value;
+  // Verifica se todos os campos foram preenchidos
+
+    if (nome && email && mensagem){
+        document.getElementById("mensagem-sucesso").style.display = "block";
+        form.reset();// Limpa os campos
+        
+    } else {
+        alert("Preencha tods os campos!");
+    }
   });
+
 
